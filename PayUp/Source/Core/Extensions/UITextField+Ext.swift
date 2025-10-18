7e60e010bb2a2897c74cb0555baa77eede3adadf
João Vitor <joaovitorcfdev@gmail.com>
2025-10-18 17:20:45 -0300

    create CustomTextField

---
+++ PayUp/Source/Core/Extensions/UITextField+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UITextField+Ext.swift
+//  PayUp
+//
+//  Created by João Vitor on 18/10/25.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+}
---
+++ PayUp/Source/Core/Models/InputTextFieldType.swift
@@ -0,0 +1,15 @@
+//
+//  InputTextFieldtype.swift
+//  PayUp
+//
+//  Created by João Vitor on 18/10/25.
+//
+
+import Foundation
+import UIKit
+
+enum InputTextFieldType {
+    case normal
+    case cellphone
+    case cnpj
+}
